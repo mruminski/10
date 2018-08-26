36439a67332d1d470fc59262210c593d9d06b369
Mateusz Rumiński <matt.ruminski@protonmail.com>
2018-08-26 16:27:19 +0200

    Set initial map center

--- js/script.js
+++ js/script.js
@@ -30,8 +30,8 @@ flkty.on('scroll', function(progress) {
 })
 
 window.initMap = function() {
-  var uluru = {lat: -25.344, lng: 131.036};
+  var petra = data[0].coords;
   var map = new google.maps.Map(
-    document.getElementById('map'), {zoom: 4, center: uluru});
-  var marker = new google.maps.Marker({position: uluru, map: map});
+    document.getElementById('map'), {zoom: 4, center: petra});
+  var marker = new google.maps.Marker({position: petra, map: map});
 }
