0ba455d4ff926204b3ea01a2c6a55c5e6766d07c
Mateusz Rumiński <matt.ruminski@protonmail.com>
2018-08-26 16:09:22 +0200

    Init google maps

--- js/script.js
+++ js/script.js
@@ -28,3 +28,10 @@ flkty.on('scroll', function(progress) {
   progress = Math.max(0, Math.min(1, progress));
   progressBar.style.width = progress * 100 + '%';
 })
+
+window.initMap = function() {
+  var uluru = {lat: -25.344, lng: 131.036};
+  var map = new google.maps.Map(
+    document.getElementById('map'), {zoom: 4, center: uluru});
+  var marker = new google.maps.Marker({position: uluru, map: map});
+}
